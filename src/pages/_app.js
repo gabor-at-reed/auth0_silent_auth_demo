0ad6458d939b21b05eb59e10ab2875b3d9afabb0
Gabor Farkas <gabor.farkas@reedonline.co.uk>
2021-09-15 13:28:08 +0100

    add UserProvider to the app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
+import {UserProvider} from '@auth0/nextjs-auth0';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider profileUrl={pageProps.profileUrl ?? '/api/auth/me'}>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp
